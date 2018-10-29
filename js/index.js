b54032f5708cfea2a5dd25e0af712b414c88d426
Oleksandr Rozhkov <alexshura@gmail.com>
2018-10-29 15:37:42 +0000

    Few changes

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
 const usersEnteredVal = prompt ("Please, enter the Fibanaci number") 
-const usersNumFiba = parseInt(((usersEnteredVal >= 1) && (parseInt(usersEnteredVal)%usersEnteredVal===0)) ? usersEnteredVal:(alert ("You are entered a wrong number, programm will with Number 1 now."),1));
+const usersNumFiba = parseInt(((usersEnteredVal >= 1) && (parseInt(usersEnteredVal)%usersEnteredVal===0)) ? usersEnteredVal:(alert ("You are entered a wrong number, programm will start with Number 1 now."),1));
 
-alert (" Method 1. Solution N 1 is :   " + methodFibaFor(usersNumFiba) + "  (rabit(s))");
-alert (" Method 2. Solution N 2 is :   " + methodFibaArr(usersNumFiba) + "  (rabit(s))");
-alert (" Method 3. Solution N 3 is :   " + methodFibaRec(usersNumFiba) + "  (rabit(s))");
-
+alert (" Method 1. Solution N 1 (For) is :   " + methodFibaFor(usersNumFiba) + "  (rabit(s))"); 
+alert (" Method 2. Solution N 2 (Arr) is :   " + methodFibaArr(usersNumFiba) + "  (rabit(s))");
+alert (" Method 3. Solution N 3 (Recur) is :   " + methodFibaRec(usersNumFiba) + "  (rabit(s))");
 
 function methodFibaFor (){
-    let firstFib=1; 
-    let secondFib=1;
+    let firstFibTmp=1; 
+    let secondFibTmp=1;
     const j = parseInt(usersNumFiba%2===0 ? (usersNumFiba/2):((usersNumFiba+1)/2));
     for (let i = 0; i < j-1; i++){
-        firstFib = firstFib + secondFib;
-        secondFib = firstFib + secondFib;
+        firstFibTmp = firstFibTmp + secondFibTmp;
+        secondFibTmp = firstFibTmp + secondFibTmp;
     }
-    return usersNumFiba%2!==0 ? firstFib:secondFib;
+    return usersNumFiba%2!==0 ? firstFibTmp:secondFibTmp;
 }   
 
 function methodFibaArr (){
@@ -26,11 +25,7 @@ function methodFibaArr (){
 }   
 
 let n = usersNumFiba;
-
 function methodFibaRec (n){
     if (n == 1 || n == 2) return 1;
     else return  methodFibaRec(n-1) + methodFibaRec(n-2);
-
 }
-
-   
